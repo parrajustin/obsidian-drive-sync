c1d3f7b81531eed7d509941bb9cc5de82a08c514
Justin Parra <1360989+parrajustin@users.noreply.github.com>
2024-09-23 00:57:59 -0700

    Add Utils for the handeling of file nodes

--- src/sync/file_node.ts
+++ src/sync/file_node.ts
@@ -1,39 +1,82 @@
-import type { TAbstractFile, Vault } from "obsidian";
+import type { App, TAbstractFile } from "obsidian";
 import { TFile } from "obsidian";
 import type { SearchString } from "../lib/search_string_parser";
 import type { Result } from "../lib/result";
 import { Err, Ok } from "../lib/result";
 import type { StatusError } from "../lib/status_error";
-import { InternalError } from "../lib/status_error";
+import { InternalError, InvalidArgumentError } from "../lib/status_error";
+import { None, Some, type Option } from "../lib/option";
+import { TypeGuard } from "../lib/type_guard";
 
+interface FileNodeParams {
+    fullPath: string;
+    ctime: number;
+    mtime: number;
+    size: number;
+    baseName: string;
+    extension: string;
+    fileId: Option<string>;
+    userId: Option<string>;
+    deleted: boolean;
+}
+
+/** File node for book keeping. */
 export class FileNode {
     /** Full filepath. */
-    public readonly fullPath: string;
+    public fullPath: string;
     /** The creation time. */
-    public readonly ctime: number;
+    public ctime: number;
     /** The modification time. */
-    public readonly mtime: number;
+    public mtime: number;
     /** Size of the file in bytes. */
-    public readonly size: number;
+    public size: number;
     /** Filename without the extension. */
-    public readonly fileName: string;
-    /** File extension (example .md). */
-    public readonly extension: string;
+    public baseName: string;
+    /** File extension (example ".md"). */
+    public extension: string;
+    /** Uid of the file. */
+    public fileId: Option<string>;
+    /** The user id of the authenticated user who made this file. */
+    public userId: Option<string>;
+    /** Only set by the firestore. */
+    public deleted: boolean;
 
-    private constructor(fullPath: string, file: TFile) {
-        this.fullPath = fullPath;
-        this.ctime = file.stat.ctime;
-        this.mtime = file.stat.mtime;
-        this.size = file.stat.size;
-        this.fileName = file.basename;
-        this.extension = file.extension;
+    constructor(config: FileNodeParams) {
+        this.fullPath = config.fullPath;
+        this.ctime = config.ctime;
+        this.mtime = config.mtime;
+        this.size = config.size;
+        this.baseName = config.baseName;
+        this.extension = config.extension;
+        this.fileId = config.fileId;
+        this.userId = config.userId;
+        this.deleted = config.deleted;
     }
 
-    /** Creates a file node. */
-    public static constructFileNode(fullPath: string, file: TFile): FileNode {
-        return new FileNode(fullPath, file);
+    /** Constructs the FileNode from TFiles. */
+    public static constructFromTFile(fullPath: string, file: TFile, fileId: Option<string>) {
+        return new FileNode({
+            fullPath,
+            ctime: file.stat.ctime,
+            mtime: file.stat.mtime,
+            size: file.stat.size,
+            baseName: file.basename,
+            extension: file.extension,
+            fileId: fileId,
+            userId: None,
+            deleted: false
+        });
+    }
+
+    public toString() {
+        return this.fullPath;
     }
 }
+
+export class FileNodeArray {
+    constructor(public nodes: FileNode[]) {}
+}
+export type FileMapOfNodes = Map<string, FileMapOfNodes | FileNodeArray>;
 
 /**
  * Checks if the `node` matches the `searchString`.
@@ -71,37 +114,53 @@ export function CheckFileNodeMatchesSearchString(
     return false;
 }
 
+/** Get the file uid from frontmatter. */
+export function GetFileUidFromFrontmatter(app: App, file: TFile): Option<string> {
+    const cache = app.metadataCache.getFileCache(file);
+    if (cache === null) {
+        return None;
+    }
+    const frontmatterCache = cache.frontmatter;
+    if (frontmatterCache === undefined) {
+        return None;
+    }
+    const uidValue = frontmatterCache["uid"];
+    if (uidValue === undefined) {
+        return None;
+    }
+    if (TypeGuard<string>(uidValue, typeof uidValue === "string" || uidValue instanceof String)) {
+        return Some(uidValue);
+    }
+    return None;
+}
+
 /** Gets all the file nodes from the filesystem. */
-export function GetAllFileNodes(vault: Vault, searchString: SearchString): FileNode[] {
+export function GetAllFileNodes(app: App, searchString: SearchString): FileNode[] {
     const files: FileNode[] = [];
 
     // First get all the files from the filemap.
-    for (const fileName in vault.fileMap) {
-        const file = vault.fileMap[fileName] as TAbstractFile;
+    for (const fileName in app.vault.fileMap) {
+        const file = app.vault.fileMap[fileName] as TAbstractFile;
         if (!(file instanceof TFile)) {
             continue;
         }
-        const node = FileNode.constructFileNode(fileName, file);
+        const fileId = GetFileUidFromFrontmatter(app, file);
+        const node = FileNode.constructFromTFile(fileName, file, fileId);
         if (!CheckFileNodeMatchesSearchString(node, searchString)) {
             continue;
         }
-        files.push();
+        files.push(node);
     }
 
     // TODO: Add .obisdian folder sync.
     return files;
 }
 
-export type FileMapOfNodes = Map<string, FileMapOfNodes | FileNode>;
+/** Converts a flat array of FileNodes to a `FileMapOfNodes`. */
+export function ConvertArrayOfNodesToMap(arry: FileNode[]): Result<FileMapOfNodes, StatusError> {
+    const mapOfNodes: FileMapOfNodes = new Map();
 
-export function GetFileMapOfNodes(
-    vault: Vault,
-    searchString: SearchString
-): Result<FileMapOfNodes, StatusError> {
-    const mapOfNodes = new Map<string, FileMapOfNodes | FileNode>();
-    const fileNodes = GetAllFileNodes(vault, searchString);
-
-    for (const node of fileNodes) {
+    for (const node of arry) {
         const pathSplit = node.fullPath.split("/");
         if (pathSplit.length === 0) {
             return Err(InternalError(`No path nodes found. "${node.fullPath}"`));
@@ -111,7 +170,7 @@ export function GetFileMapOfNodes(
         let folderNode = mapOfNodes;
         for (const path of pathSplit) {
             if (!folderNode.has(path)) {
-                folderNode.set(path, new Map<string, FileMapOfNodes | FileNode>());
+                folderNode.set(path, new Map<string, FileMapOfNodes | FileNodeArray>());
             }
             const selectedFolderNode = folderNode.get(path);
             if (selectedFolderNode === undefined) {
@@ -119,17 +178,110 @@ export function GetFileMapOfNodes(
                     InternalError(`GetFileMapOfNodes path "${node.fullPath}" somehow undefined!`)
                 );
             }
-            if (selectedFolderNode instanceof FileNode) {
+            if (selectedFolderNode instanceof FileNodeArray) {
                 return Err(
                     InternalError(
-                        `GetFileMapOfNodes found a node of file when expecting a folder "${node.fullPath}".`
+                        `GetFileMapOfNodes found a file array when expecting a folder "${node.fullPath}".`
                     )
                 );
             }
             folderNode = selectedFolderNode;
         }
-        folderNode.set(finalFileName, node);
+
+        const folderArray = folderNode.get(finalFileName);
+        if (folderArray === undefined) {
+            folderNode.set(finalFileName, new FileNodeArray([node]));
+        } else if (folderArray instanceof FileNodeArray) {
+            folderArray.nodes.push(node);
+            // Validate to make sure there aren't more than 1 not deleted file at a file path.
+            let countNonDeleted = 0;
+            for (const deletedCheckNode of folderArray.nodes) {
+                if (!deletedCheckNode.deleted) {
+                    countNonDeleted++;
+                }
+            }
+            if (countNonDeleted > 1) {
+                return Err(
+                    InternalError(`Found multiple not deleted files at "${node.fullPath}".`)
+                );
+            }
+        } else {
+            return Err(InternalError(`Found a folder at final path for "${node.fullPath}".`));
+        }
     }
 
     return Ok(mapOfNodes);
+}
+
+/** Get the map of nodes or files. Use to keep track of file changes. */
+export function GetFileMapOfNodes(
+    app: App,
+    searchString: SearchString
+): Result<FileMapOfNodes, StatusError> {
+    const fileNodes = GetAllFileNodes(app, searchString);
+    return ConvertArrayOfNodesToMap(fileNodes);
+}
+
+/** Flattens the file map to get the array of file nodes. */
+export function FlattenFileNodes(fileMap: FileMapOfNodes): FileNode[] {
+    const fileNodes: FileNode[] = [];
+
+    const recursiveCheck = (map: FileMapOfNodes) => {
+        for (const [_key, value] of map) {
+            if (value instanceof FileNodeArray) {
+                fileNodes.push(...value.nodes);
+            } else {
+                recursiveCheck(value);
+            }
+        }
+    };
+    recursiveCheck(fileMap);
+    return fileNodes;
+}
+
+/** Gets a map of the nodes keyed by their file-id. */
+export function MapByFileId(arry: FileNode[]): Map<string, FileNode> {
+    const map = new Map<string, FileNode>();
+    for (const node of arry) {
+        if (node.fileId.none) {
+            continue;
+        }
+
+        map.set(node.fileId.safeValue(), node);
+    }
+    return map;
+}
+
+/** Get the non deleted file at a file path. */
+export function GetNonDeletedByFilePath(
+    fileMap: FileMapOfNodes,
+    filePath: string
+): Result<Option<FileNode>, StatusError> {
+    const pathSegments = filePath.split("/");
+    if (pathSegments.length === 0) {
+        return Err(InvalidArgumentError("FilePath is required."));
+    }
+    const fileName = pathSegments.pop() as string;
+    let selectedFileNode = fileMap;
+    for (const path of pathSegments) {
+        const node = selectedFileNode.get(path);
+        if (node === undefined) {
+            return Err(InvalidArgumentError(`path "${filePath}" somehow undefined!`));
+        } else if (node instanceof FileNodeArray) {
+            return Err(InvalidArgumentError(`path "${filePath}" leads to leaf node.`));
+        }
+        selectedFileNode = node;
+    }
+    const expectingNodeArry = selectedFileNode.get(fileName);
+    if (expectingNodeArry === undefined || !(expectingNodeArry instanceof FileNodeArray)) {
+        return Err(InvalidArgumentError(`path "${filePath}" does not lead to a leaf node.`));
+    }
+    // Ensure there is only a single not deleted node.
+    const nonDeletedNodes = expectingNodeArry.nodes.filter((node) => !node.deleted);
+    if (nonDeletedNodes.length > 1) {
+        return Err(InternalError(`path "${filePath}" leads to several non deleted nodes!`));
+    } else if (nonDeletedNodes.length === 0) {
+        return Ok(None);
+    }
+    return Ok(Some(nonDeletedNodes[0] as FileNode));
 }
