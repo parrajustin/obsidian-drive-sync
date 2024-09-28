da815ae4ee51b0d86ffb7bea596a195667ff7cf6
Justin Parra <1360989+parrajustin@users.noreply.github.com>
2024-09-27 23:47:39 -0700

    fix: update version bump build script

--- version-bump.mjs
+++ version-bump.mjs
@@ -5,7 +5,7 @@ const targetVersion = process.env.npm_package_version;
 // read minAppVersion from manifest.json and bump version to target version
 let manifest = JSON.parse(readFileSync('manifest.json', 'utf8'));
 const { minAppVersion } = manifest;
-manifest.version = targetVersion;
+manifest.version = `v${targetVersion}`;
 writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'));
 
 // update versions.json with target version and minAppVersion from manifest.json
