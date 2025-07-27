e8bdd9e2a8b4bad6a0cb3410bc1d639354588969
Justin <1360989+parrajustin@users.noreply.github.com>
2025-07-27 00:25:33 +0000

    feat: Add a map util
    Just adds a small util to convert an array of FileNodes to a map to make it easier to find the necessary FileNode.

---
+++ src/filesystem/file_map_util.ts
@@ -0,0 +1,15 @@
+import { Span } from "../logging/tracing/span.decorator";
+import { FileNode, FilePathType } from "./file_node";
+
+export type MapOfFileNodes = Map<FilePathType, FileNode>;
+
+export class FileMapUtil {
+    @Span()
+    public static convertNodeToMap(allNodes: FileNode[]): MapOfFileNodes {
+        const map = new Map<FilePathType, FileNode>();
+        for (const node of allNodes) {
+            map.set(node.filedata.fullPath, node);
+        }
+        return map;
+    }
+}
